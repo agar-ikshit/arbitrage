092bdd5ceea2048ff2083940a839e9f40c9552c8
agar-ikshit <ikshitagarwa@gmail.com>
2025-05-16 11:16:42 +0530

    binace api fixed for cron job

--- exchanges/binance.py
+++ exchanges/binance.py
@@ -1,32 +1,25 @@
 import requests
 from .models import ExchangeDataResponse
 import logging
+
 logger = logging.getLogger(__name__)
 
-
-
-
-
 def get_usd_orderbook(coin: str, currency: str = "USDT") -> ExchangeDataResponse | None:
-
     symbol = f"{coin.upper()}{currency.upper()}"
- 
-    base_url = "https://api.binance.com/api/v3/depth"
+    url = f"https://api.binance.com/api/v3/ticker/bookTicker"
+
     params = {
-        "symbol": symbol,
-        "limit": 1
+        "symbol": symbol
     }
-    
 
     try:
-        response = requests.get(base_url, params=params)
-        
+        response = requests.get(url, params=params)
         response.raise_for_status()
         data = response.json()
-        
-        if "bids" in data and "asks" in data and data["bids"] and data["asks"]:
-            best_bid_price = float(data["bids"][0][0])
-            best_ask_price = float(data["asks"][0][0])
+
+        if "bidPrice" in data and "askPrice" in data:
+            best_bid_price = float(data["bidPrice"])
+            best_ask_price = float(data["askPrice"])
 
             return ExchangeDataResponse(
                 exchange="Binance",
@@ -35,14 +28,13 @@ def get_usd_orderbook(coin: str, currency: str = "USDT") -> ExchangeDataResponse
                 best_ask_price=best_ask_price
             )
         else:
-            print("Error or empty order book:", data)
+            print("Unexpected response format:", data)
             return None
     except requests.RequestException as e:
         print(f"Error fetching Binance data: {e}")
         print(symbol)
         print(coin)
         return None
-
 
 if __name__ == "__main__":
     print(get_usd_orderbook("BTC"))
